843186c70015577771afd3e5109195294c6606b9
David Kanekanian <dkanekanian1@gmail.com>
2025-03-09 16:34:29 +0000

    recyelrviewadapter

--- qt/gallery.py
+++ qt/gallery.py
@@ -1,3 +1,6 @@
+from abc import abstractmethod
+from collections import abc
+from typing import Optional
 from PySide6 import QtCore, QtWidgets, QtGui
 
 
@@ -11,11 +14,81 @@ def generate_image(size: QtCore.QSize, number: int):
     return image
 
 
-class RecyclerView(QtWidgets.QWidget):
+class RecyclerViewAdapter(metaclass=abc.ABCMeta):
+    @abstractmethod
+    def create_view(self) -> QtWidgets.QWidget:
+        """Create a fresh, empty item.
+
+        Its size hint will indicate how much space to allocate for the items.
+        """
+    
+    @abstractmethod
+    def bind_view(self, view: QtWidgets.QWidget, index: int) -> None:
+        """Bind a widget, potentially one that is being recycled."""
+    
+    @abstractmethod
+    def get_size(self) -> int:
+        """Return the number of items in the dataset."""
+
+
+class RecyclerView(QtWidgets.QScrollArea):
     """A scrollable container used to efficiently show a large number of items."""
 
     def __init__(self):
         super().__init__()
+        self._adapter: Optional[RecyclerViewAdapter] = None
+
+        # TODO is this needed?
+        self.setWidgetResizable(True)
+
+        inner = QtWidgets.QWidget()
+        self.setWidget(inner)
+
+        # TODO list only for now, later grid too
+        vbox1 = QtWidgets.QVBoxLayout(inner)
+        vbox1.setContentsMargins(0, 0, 0, 0)
+        vbox1.setSpacing(0)
+
+    def set_adapter(self, adapter: RecyclerViewAdapter):
+        self._adapter = adapter
+
+        view = self._adapter.create_view()
+        total_height = view.sizeHint().height() * self._adapter.get_size()
+        self.widget().setFixedHeight(total_height)
+
+    def scrollContentsBy(self, dx, dy):
+        # Called when the scroll area is scrolled.
+        super().scrollContentsBy(dx, dy)
+
+
+class MyListAdapter(RecyclerViewAdapter):
+    def __init__(self, data):
+        self.data = data
+
+    def create_view(self) -> QtWidgets.QWidget:
+        label = QtWidgets.QLabel()
+        label.setFixedHeight(50)
+        return label
+    
+    def bind_view(self, view: QtWidgets.QWidget, index: int) -> None:
+        view.setText(f"Item {index}")
+    
+    def get_size(self) -> int:
+        return len(self.data)
+
+class MyList(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        vbox1 = QtWidgets.QVBoxLayout(self)
+        vbox1.setContentsMargins(0, 0, 0, 0)
+        vbox1.setSpacing(0)
+
+        recycler_view = RecyclerView()
+        data = [f"Item {i}" for i in range(100)]
+        adapter = MyListAdapter(data)
+        recycler_view.set_adapter(adapter)
+        vbox1.addWidget(recycler_view)
 
 
 class Gallery(QtWidgets.QWidget):
@@ -64,9 +137,13 @@ def main():
     # window = QtWidgets.QWidget()
     # window.show()
 
-    gallery = Gallery()
-    gallery.setMinimumSize(800, 600)
-    gallery.show()
+    my_list = MyList()
+    my_list.setMinimumSize(800, 600)
+    my_list.show()
+
+    # gallery = Gallery()
+    # gallery.setMinimumSize(800, 600)
+    # gallery.show()
 
     app.exec()
 
